19c82a81662b166577c54ca9be1477a2b29f00d2
Felipe Skroski <felipeskroski@gmail.com>
2014-11-07 17:07:42 +1300

    first commit

---
+++ gruntfile.js
@@ -0,0 +1,61 @@
+module.exports = function(grunt) {
+
+grunt.initConfig({
+  less: {
+    production: {
+      options: {
+        paths: ["bower_components/bootstrap/less"],
+        yuicompress: true
+      },
+      files: {
+        "assets/css/bootstrap.min.css": "bower_components/bootstrap/dist/css/bootstrap.min.css"
+      }
+    }
+  },
+  uglify: {
+    jquery: {
+      files: {
+        'assets/js/jquery.min.js': 'bower_components/jquery/jquery.js'
+      }
+    },
+    bootstrap: {
+      files: {
+        'assets/js/bootstrap.min.js': ['bower_components/bootstrap/js/collapse.js',
+                                       'bower_components/bootstrap/js/scrollspy.js',
+                                       'bower_components/bootstrap/js/button.js',
+                                       'bower_components/bootstrap/js/affix.js']
+      }
+    }
+  },
+  css: {
+    
+  },
+  copy: {
+    bootstrap: {
+      files: [
+        {expand: true, cwd: 'bower_components/bootstrap/img/', src: ['**'], dest: 'assets/img/'}
+      ]
+    }
+  },
+  exec: {
+    build: {
+      cmd: 'jekyll build'
+    },
+    serve: {
+      cmd: 'jekyll serve --watch'
+    },
+    deploy: {
+      cmd: 'rsync --progress -a --delete -e "ssh -q" _site/ myuser@host:mydir/'
+    }
+  }
+});
+
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-less');
+grunt.loadNpmTasks('grunt-contrib-copy');
+grunt.loadNpmTasks('grunt-exec');
+
+grunt.registerTask('default', [ 'less', 'uglify', 'copy', 'exec:build' ]);
+grunt.registerTask('deploy', [ 'default', 'exec:deploy' ]);
+
+};
